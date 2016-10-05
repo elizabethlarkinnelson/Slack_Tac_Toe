32abeb07708dfb505b1861d9ae048a0c0355768b
Elizabeth Nelson <elizabeth.larkin.nelson@gmail.com>
2016-10-05 18:45:05 +0000

    Slash command working with server/ngrok. Next step to start game code

--- server.py
+++ server.py
@@ -1,18 +1,32 @@
-from flask import Flask, request, render_template
+import os
+
+from flask import Flask, request, Response
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 
 app.secret_key = "ABC"
 
-
-@app.route('/')
-def index():
-    """Homepage"""
-
-    return render_template('test.html')
+SLACK_WEBHOOK_SECRET = os.environ.get('SLACK_WEBHOOK_SECRET')
 
 
+@app.route('/slack', methods=['POST'])
+def inbound():
+    if request.form.get('token') == SLACK_WEBHOOK_SECRET:
+        #USED FOR TESTING
+        # channel = request.form.get('channel_name')
+        # username = request.form.get('user_name')
+        # text = request.form.get('text')
+        # inbound_message = username + " in " + channel + " says: " + text
+        # print inbound_message
+
+        #CHALLENGE CODE:
+        
+    return Response(), 200
+
+@app.route('/', methods=['GET'])
+def test():
+    return Response('It works!')
 
 if __name__ == "__main__":
 
