733122231bcfe597e107f054af41d1adf4f00274
Elizabeth Nelson <elizabeth.larkin.nelson@gmail.com>
2016-10-14 21:32:53 +0000

    db create all

--- model.py
+++ model.py
@@ -304,6 +304,7 @@ def connect_to_db(app, db_uri=None):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
+    db.create_all()
 
 if __name__ == "__main__":
     #To utilize database interactively
